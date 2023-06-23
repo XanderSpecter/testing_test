cec66892da3e960b55616b82855b45dc0a8b9bc3
Alexander Bogdanov <sbogdanov1993@gmail.com>
2023-06-23 20:07:54 +0500

    Исправлены тексты при подключении к БД

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+    throw new Error('Укажи блять ёбаный путь до сервера БД в файле .env.local');
 }
 
 const client = new MongoClient(MONGODB_URI);
@@ -11,7 +11,7 @@ const dbName = 'test';
 
 export async function connect() {
     await client.connect();
-    console.log('Connected successfully to server');
+    console.log('Ебаная база данных теперь доступна');
     const db = client.db(dbName);
     const collection = db.collection('page');
 
