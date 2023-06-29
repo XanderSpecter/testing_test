68d5cd9c9a3b7055f3a760766fb71c0bb95de43d
Alexander Bogdanov <sbogdanov1993@gmail.com>
2023-06-29 11:19:04 +0500

    Добавлена обработка 404 для недоступных коллекций

--- src/app/admin/[collectionElementName]/page.tsx
+++ src/app/admin/[collectionElementName]/page.tsx
@@ -1,5 +1,6 @@
 import { dehydrate } from '@tanstack/query-core';
 import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 
 import { getElements } from '@/api/collection';
 import AdminCollection from '@/components/views/AdminCollection';
@@ -8,6 +9,7 @@ import Hydrate from '@/utils/queryClient/hidrateClient';
 import { BaseObject, Collection } from '@/types/apiModels';
 import { DEFAULT_APP_TITLE } from '@/constants/appParams';
 import { AVAILABLE_COLLECTIONS } from '@/constants/collections';
+import checkIsCollectionAvailable from '@/backend/checkIsCollectionAvailable';
 
 interface CollectionPageProps {
     params: Collection;
@@ -17,7 +19,9 @@ interface CollectionPageProps {
 export async function generateMetadata({ params }: CollectionPageProps): Promise<Metadata> {
     const { collectionElementName } = params;
 
-    const collectionTitle = AVAILABLE_COLLECTIONS.find((c) => c.name === collectionElementName)?.title;
+    const collectionTitle =
+        AVAILABLE_COLLECTIONS.find((c) => c.name === collectionElementName)?.title ||
+        `Коллекция ${collectionElementName}`;
 
     return {
         title: `${collectionTitle}-${DEFAULT_APP_TITLE}`,
@@ -26,6 +30,12 @@ export async function generateMetadata({ params }: CollectionPageProps): Promise
 
 export default async function CollectionPage({ params, searchParams }: CollectionPageProps) {
     const { collectionElementName } = params;
+
+    const isCollectionAvailable = checkIsCollectionAvailable(collectionElementName);
+
+    if (!isCollectionAvailable) {
+        notFound();
+    }
 
     const queryClient = getQueryClient();
     await queryClient.prefetchQuery([collectionElementName], () =>
