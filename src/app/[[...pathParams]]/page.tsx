a4cbb45fb9997f14bf79530b964353fa37fba15c
Alexander Bogdanov <sbogdanov1993@gmail.com>
2023-07-06 16:30:58 +0500

    Добавлен title

--- src/app/[[...pathParams]]/page.tsx
+++ src/app/[[...pathParams]]/page.tsx
@@ -1,6 +1,7 @@
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { getElements } from '@/api/collection';
-import { BASE_URL } from '@/constants/appParams';
+import { BASE_URL, DEFAULT_APP_TITLE } from '@/constants/appParams';
 import Renderer from '@/components/system/Renderer';
 import { BaseObject } from '@/types/apiModels';
 import { concatUrlFromPathParams } from '@/utils/url';
@@ -13,6 +14,22 @@ interface Path {
 interface HomeParams {
     params: Path;
     searchParams: BaseObject;
+}
+
+export async function generateMetadata({ params }: HomeParams): Promise<Metadata> {
+    const { pathParams } = params;
+
+    const url = concatUrlFromPathParams(BASE_URL, pathParams);
+
+    const pages = await getElements({ collectionElementName: 'page', query: { url } });
+
+    if (pages && pages.length === 1 && pages[0].name) {
+        return { title: `${pages[0].name}-${DEFAULT_APP_TITLE}` };
+    }
+
+    return {
+        title: DEFAULT_APP_TITLE,
+    };
 }
 
 export default async function Home({ params }: HomeParams) {
