77d4fb315b7f90dae162cc958a3b2477a5eac42f
Alexander Bogdanov <sbogdanov1993@gmail.com>
2023-07-07 19:08:38 +0500

    Удалены лишние дескрипшны

--- src/constants/pageBlocks.ts
+++ src/constants/pageBlocks.ts
@@ -4,18 +4,14 @@ export const PAGE_BLOCK_TYPES = [
     {
         label: 'HTML-элемент',
         value: ElementType.HTMLELEMENT,
-        description: 'Элемент разметки, с поддержкой настройки стилей, позиционирования и т.д.',
     },
     {
         label: 'Текст',
         value: ElementType.TEXT,
-        description: 'Простой текст, без гибкой настройки стилей. Может быть дочерним элементом в HTML-элементе',
     },
     {
         label: 'React-компонент',
         value: ElementType.COMPONENT,
         disabled: true,
-        description:
-            'Полноценный компонент, содержащий в себе стили и внутреннюю логику, например, текстовое поле ввода (в разработке)',
     },
 ];
